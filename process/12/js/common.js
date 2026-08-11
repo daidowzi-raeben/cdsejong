5dc193ae027c552b2c2f5306798a96bd2f8e65f7
장영욱 <raeben@naver.com>
2026-08-11 10:28:41 +0900

    fix

--- process/12/js/common.js
+++ process/12/js/common.js
@@ -1,94 +1,94 @@
-$(function(){
+$(function () {
 
-  
+
   // 다운로드 창
-  $('.u_download').bind('click',function(){
-    if ($(this).is('.on')){
+  $('.u_download').bind('click', function () {
+    if ($(this).is('.on')) {
       $('#tgDown').fadeOut(300);
       $(this).removeClass('on');
-      if ($('.quiz').length > 0 || $('.jp-limit').length > 0){}else{$('#jquery_jplayer_1').jPlayer('play');}
+      if ($('.quiz').length > 0 || $('.jp-limit').length > 0) { } else { $('#jquery_jplayer_1').jPlayer('play'); }
     } else {
       $('#tgDown').fadeIn(300);
       $(this).addClass('on');
-      if ($('.quiz').length > 0 || $('.jp-limit').length > 0){}else{$('#jquery_jplayer_1').jPlayer('pause');}
+      if ($('.quiz').length > 0 || $('.jp-limit').length > 0) { } else { $('#jquery_jplayer_1').jPlayer('pause'); }
     }
   });
 
 
-  
+
   // 인덱스 창
-  $('.u_index').bind('click',function(){
+  $('.u_index').bind('click', function () {
     var h = $('#navigation').outerHeight();
-    if ($(this).is('.on')){
-      $('#indexNavi').stop().animate({'height':'0'},400);
+    if ($(this).is('.on')) {
+      $('#indexNavi').stop().animate({ 'height': '0' }, 400);
       $(this).removeClass('on');
-    //  if ($('.quiz').length > 0 || $('.jp-limit').length > 0){}else{$('#jquery_jplayer_1').jPlayer('play');}
+      //  if ($('.quiz').length > 0 || $('.jp-limit').length > 0){}else{$('#jquery_jplayer_1').jPlayer('play');}
     } else {
-      $('#indexNavi').stop().animate({'height':h},400);
+      $('#indexNavi').stop().animate({ 'height': h }, 400);
       $(this).addClass('on');
-     // if ($('.quiz').length > 0 || $('.jp-limit').length > 0){}else{$('#jquery_jplayer_1').jPlayer('pause');}
-    }
-  });  
-
-
- 
-  // 스크립트 창
-  $('.jp-script').bind('click',function(){
-
-    var h = $('.scHead').height()+$('.scConfBox').outerHeight();
-    if ($(this).is('.on')){
-      $('#scriptBox').stop().animate({'height':'0'},400);
-      $(this).removeClass('on');
-    //  if ($('.quiz').length > 0){}else{$('#jquery_jplayer_1').jPlayer('play');}
-    } else {
-      $('#scriptBox').stop().animate({'height':h},400);
-      $(this).addClass('on');
-    //  if ($('.quiz').length > 0){}else{$('#jquery_jplayer_1').jPlayer('pause');}
+      // if ($('.quiz').length > 0 || $('.jp-limit').length > 0){}else{$('#jquery_jplayer_1').jPlayer('pause');}
     }
   });
-  $('.scClose').bind('click',function(){
+
+
+
+  // 스크립트 창
+  $('.jp-script').bind('click', function () {
+
+    var h = $('.scHead').height() + $('.scConfBox').outerHeight();
+    if ($(this).is('.on')) {
+      $('#scriptBox').stop().animate({ 'height': '0' }, 400);
+      $(this).removeClass('on');
+      //  if ($('.quiz').length > 0){}else{$('#jquery_jplayer_1').jPlayer('play');}
+    } else {
+      $('#scriptBox').stop().animate({ 'height': h }, 400);
+      $(this).addClass('on');
+      //  if ($('.quiz').length > 0){}else{$('#jquery_jplayer_1').jPlayer('pause');}
+    }
+  });
+  $('.scClose').bind('click', function () {
     $('.jp-script').click();
   });
 
 
   // 창숨기기
-  $(document).mouseup(function(e){
-    if ($('.movieView').hasClass('on')){
+  $(document).mouseup(function (e) {
+    if ($('.movieView').hasClass('on')) {
       return false;
     } else {
 
 
-      if (!$('#tgDown, .u_download').is(e.target) && $('#tgDown, .u_download').has(e.target).length === 0){
-        if($('.u_download').is('.on')){
+      if (!$('#tgDown, .u_download').is(e.target) && $('#tgDown, .u_download').has(e.target).length === 0) {
+        if ($('.u_download').is('.on')) {
           $('#tgDown').fadeOut(300);
           $('.u_download').removeClass('on');
-          if ($('.quiz').length > 0 || $('.jp-limit').length > 0){}else{$('#jquery_jplayer_1').jPlayer('play');}
+          if ($('.quiz').length > 0 || $('.jp-limit').length > 0) { } else { $('#jquery_jplayer_1').jPlayer('play'); }
         }
       }
 
-      if (!$('#tgMovie, .u_playmovie').is(e.target) && $('#tgMovie, .u_playmovie').has(e.target).length === 0){
-        if($('.u_playmovie').is('.on')){
+      if (!$('#tgMovie, .u_playmovie').is(e.target) && $('#tgMovie, .u_playmovie').has(e.target).length === 0) {
+        if ($('.u_playmovie').is('.on')) {
           $('#tgMovie').fadeOut(300);
           $('.u_playmovie').removeClass('on');
-          if ($('.quiz').length > 0 || $('.jp-limit').length > 0){}else{$('#jquery_jplayer_1').jPlayer('play');}
+          if ($('.quiz').length > 0 || $('.jp-limit').length > 0) { } else { $('#jquery_jplayer_1').jPlayer('play'); }
         }
       }
 
-      if (!$('#scriptBox, .jp-script').is(e.target) && $('#scriptBox, .jp-script').has(e.target).length === 0){
-        if($('.jp-script').is('.on')){
-          var h = $('.scHead').height()+$('.scConfBox').outerHeight();
-          $('#scriptBox').stop().animate({'height':'0'},400);
+      if (!$('#scriptBox, .jp-script').is(e.target) && $('#scriptBox, .jp-script').has(e.target).length === 0) {
+        if ($('.jp-script').is('.on')) {
+          var h = $('.scHead').height() + $('.scConfBox').outerHeight();
+          $('#scriptBox').stop().animate({ 'height': '0' }, 400);
           $('.jp-script').removeClass('on');
-          if ($('.quiz').length > 0 || $('.jp-limit').length > 0){}else{$('#jquery_jplayer_1').jPlayer('play');}
+          if ($('.quiz').length > 0 || $('.jp-limit').length > 0) { } else { $('#jquery_jplayer_1').jPlayer('play'); }
         }
       }
 
-      if (!$('#navigation, .u_index').is(e.target) && $('#navigation, .u_index').has(e.target).length === 0){
-        if($('.u_index').is('.on')){
+      if (!$('#navigation, .u_index').is(e.target) && $('#navigation, .u_index').has(e.target).length === 0) {
+        if ($('.u_index').is('.on')) {
           var h = $('#navigation').outerHeight();
-          $('#indexNavi').stop().animate({'height':'0'},400);
+          $('#indexNavi').stop().animate({ 'height': '0' }, 400);
           $('.u_index').removeClass('on');
-          if ($('.quiz').length > 0 || $('.jp-limit').length > 0){}else{$('#jquery_jplayer_1').jPlayer('play');}
+          if ($('.quiz').length > 0 || $('.jp-limit').length > 0) { } else { $('#jquery_jplayer_1').jPlayer('play'); }
         }
       }
     }
@@ -101,38 +101,38 @@ $(function(){
 });
 
 
-/*************************************************************************************************/ 
+/*************************************************************************************************/
 // 진도체크
-/*************************************************************************************************/ 
+/*************************************************************************************************/
 var makeName = ".html";
 var nowIndex = document.URL.split(makeName)[0];
-var chapter = nowIndex.substring(nowIndex.length-6,nowIndex.length-4);
-var page = nowIndex.substring(nowIndex.length-2,nowIndex.length);
+var chapter = nowIndex.substring(nowIndex.length - 6, nowIndex.length - 4);
+var page = nowIndex.substring(nowIndex.length - 2, nowIndex.length);
 
 //진도 체크
-p = parseInt(page,10);
-parent.pageCheck( p );
+p = parseInt(page, 10);
+parent.pageCheck(p);
 
-//var mURL = "../mp4/";
-var mURL = "http://nstest.sejong21.co.kr/process/ethic2209/mp4/";
+var mURL = "../mp4/";
+//var mURL = "http://nstest.sejong21.co.kr/process/ethic2209/mp4/";
 var mp4_Name = mURL + chapter + "_" + page + ".mp4";
 
 var topTitle = "나와 조직을 지키는 청렴윤리 가이드라인";	// 브라우저 타이틀
 document.title = topTitle;
 
 function pageInit() {
-	try{
-		var pageInfo = chapter + "_" + page;
-		//alert(pageInfo)
-		top._getParagraph();
-		top._setPageInfo(pageInfo); //장 절
-		top._setCurrentLocation(page); //현재 페이지
-		top._progressSave();
-		
-				
-	}catch(e){
-		//alert("페이지 정보 저장 시 오류가 발생 함");
-	}
-	
+  try {
+    var pageInfo = chapter + "_" + page;
+    //alert(pageInfo)
+    top._getParagraph();
+    top._setPageInfo(pageInfo); //장 절
+    top._setCurrentLocation(page); //현재 페이지
+    top._progressSave();
+
+
+  } catch (e) {
+    //alert("페이지 정보 저장 시 오류가 발생 함");
+  }
+
 }
 /*************************************************************************************************/
